c4aee4631413610d8fa4c4ffc0cff7db8d1ad03d
Azatyk <azatuk2005@mail.ru>
2021-10-26 21:53:53 +0600

    feat: edit login page

--- lib/onboarding/views/login_view.dart
+++ lib/onboarding/views/login_view.dart
@@ -23,65 +23,69 @@ class _LoginViewState extends State<LoginView> {
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Stack(
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 25),
-                  GestureDetector(
-                    onTap: () => {Navigator.pop(context)},
-                    child: Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          FaIcon(
-                            FontAwesomeIcons.arrowLeft,
-                            size: 16,
-                            color: AppColors.primaryColor,
-                          ),
-                          SizedBox(width: 10),
-                          Text(
-                            "Назад",
-                            style: TextStyle(
-                                color: AppColors.primaryColor, fontSize: 18),
-                          )
-                        ],
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 25),
+                    GestureDetector(
+                      onTap: () => {Navigator.pop(context)},
+                      child: Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.arrowLeft,
+                              size: 16,
+                              color: AppColors.primaryColor,
+                            ),
+                            SizedBox(width: 10),
+                            Text(
+                              "Назад",
+                              style: TextStyle(
+                                  color: AppColors.primaryColor, fontSize: 18),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-                  SizedBox(
-                    width: 300,
-                    child: Text(
-                      "С возвращением! Войдите в аккаунт",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: AppColors.blackColor,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w500,
-                          height: 1.3),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        "С возвращением! Войдите в аккаунт",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColors.blackColor,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w500,
+                            height: 1.3),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Input(
-                      title: "Номер телефона",
-                      hintText: "+7 (000) 000 00-00",
-                      phoneInput: true,
-                      updateValue: (text) {}),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Input(
-                      title: "Пароль",
-                      hintText: "Введите пароль",
-                      passwordInput: true,
-                      updateValue: (text) {})
-                ],
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Input(
+                        title: "Номер телефона",
+                        hintText: "+7 (000) 000 00-00",
+                        phoneInput: true,
+                        hasTitle: true,
+                        updateValue: (text) {}),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Input(
+                        title: "Пароль",
+                        hintText: "Введите пароль",
+                        passwordInput: true,
+                        hasTitle: true,
+                        updateValue: (text) {})
+                  ],
+                ),
               ),
               Align(
                 alignment: Alignment.bottomCenter,
