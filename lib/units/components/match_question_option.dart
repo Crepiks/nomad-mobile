078b740d2cbca7a1235777261d6c8d08f3397229
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-21 20:03:21 +0600

    feat: display list of available options in bottom sheet for match questions

---
+++ lib/units/components/match_question_option.dart
@@ -0,0 +1,50 @@
+import "package:flutter/cupertino.dart";
+import 'package:nomad/common/constants/app_colors.dart';
+
+typedef TapAction = Function();
+
+class MatchQuestionOption extends StatelessWidget {
+  final String text;
+  final bool selected;
+  final TapAction? onTap;
+
+  const MatchQuestionOption(
+      {Key? key, required this.text, this.selected = false, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap!();
+        }
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        decoration: const BoxDecoration(
+            color: AppColors.background,
+            borderRadius: BorderRadius.all(Radius.circular(12))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            selected ? _buildSelectedBullet() : Container()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSelectedBullet() {
+    return Container(
+      height: 10,
+      width: 10,
+      decoration:
+          const BoxDecoration(color: AppColors.primary, shape: BoxShape.circle),
+    );
+  }
+}
--- lib/units/components/renderers/questions/match_questions_renderer.dart
+++ lib/units/components/renderers/questions/match_questions_renderer.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:nomad/common/constants/app_colors.dart';
 import 'package:nomad/data/models/questions/match_question.dart';
 import 'package:nomad/data/models/questions/question.dart';
+import 'package:nomad/units/components/match_question_option.dart';
 
 class MatchQuestionsRenderer extends StatefulWidget {
   final List<Question> questions;
@@ -39,6 +40,13 @@ class _MatchQuestionsRendererState extends State<MatchQuestionsRenderer> {
     );
   }
 
+  List<String> _getAnswers(List<Question> questions) {
+    return questions.map((Question question) {
+      final matchQuestion = question as MatchQuestion;
+      return matchQuestion.answer;
+    }).toList();
+  }
+
   List<Widget> _buildQuestions(List<Question> questions) {
     return questions.asMap().entries.map((element) {
       final MatchQuestion question = element.value as MatchQuestion;
@@ -47,12 +55,18 @@ class _MatchQuestionsRendererState extends State<MatchQuestionsRenderer> {
 
       return Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
-        child: _buildMatchQuestion(text: questionText, index: index),
+        child: _buildMatchQuestion(
+            text: questionText,
+            index: index,
+            onTap: () {
+              _showOptionsBottomSheet(context);
+            }),
       );
     }).toList();
   }
 
-  Widget _buildMatchQuestion({required int index, required String text}) {
+  Widget _buildMatchQuestion(
+      {required int index, required String text, required onTap}) {
     return SizedBox(
       width: double.infinity,
       child: Column(
@@ -63,7 +77,9 @@ class _MatchQuestionsRendererState extends State<MatchQuestionsRenderer> {
                   color: AppColors.black, fontSize: 18, height: 1.2)),
           const SizedBox(height: 10),
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              onTap();
+            },
             child: Container(
                 width: double.infinity,
                 height: 56,
@@ -79,5 +95,49 @@ class _MatchQuestionsRendererState extends State<MatchQuestionsRenderer> {
         ],
       ),
     );
+  }
+
+  _showOptionsBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        backgroundColor: AppColors.white,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(26))),
+        builder: (BuildContext context) {
+          return Container(
+              padding: const EdgeInsets.only(right: 20, left: 20),
+              child: ListView(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  const Text("Выберите вариант",
+                      style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.black)),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  ..._buildMatchQuestionOptions()
+                ],
+              ));
+        });
+  }
+
+  List<Widget> _buildMatchQuestionOptions() {
+    final answers = _getAnswers(widget.questions);
+    return answers
+        .map(
+          (String answer) => Padding(
+            padding: const EdgeInsets.only(bottom: 14),
+            child: MatchQuestionOption(
+              text: answer,
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        )
+        .toList();
   }
 }
