6971122d9b2a2f9a3ffb8a3beec65ca825bf10c9
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-21 12:44:50 +0600

    feat: remove dotted border package

--- lib/common/renderers/content_box_renderer.dart
+++ lib/common/renderers/content_box_renderer.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:dotted_border/dotted_border.dart';
 import 'package:nomad/common/constants/app_colors.dart';
 import 'package:nomad/data/models/theory_border_box.dart';
 import 'package:nomad/units/components/theory_item_renderer.dart';
@@ -11,24 +10,16 @@ class ContentBoxRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DottedBorder(
-      borderType: BorderType.RRect,
-      color: AppColors.primary,
-      dashPattern: const [14, 6],
-      strokeWidth: 3,
-      radius: const Radius.circular(14),
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 24),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: AppColors.white,
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+      decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(14),
-        ),
-        child: Column(
-          children: content.items
-              .map((item) => TheoryItemRenderer(content: item))
-              .toList(),
-        ),
+          border: Border.all(color: AppColors.primary, width: 2)),
+      child: Column(
+        children: content.items
+            .map((item) => TheoryItemRenderer(content: item))
+            .toList(),
       ),
     );
   }
