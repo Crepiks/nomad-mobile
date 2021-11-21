37fcee6ba151d0327fe9a507290efeb1826da1a2
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-21 13:46:00 +0600

    refactor: move rows and cells build code into separate methods

--- lib/common/renderers/table_renderer.dart
+++ lib/common/renderers/table_renderer.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import 'package:nomad/common/constants/app_colors.dart';
 import 'package:nomad/data/models/theory_table.dart';
+import 'package:nomad/data/models/theory_table_cell.dart';
+import 'package:nomad/data/models/theory_table_row.dart';
 import 'package:nomad/units/components/theory_item_renderer.dart';
 
 class TableRenderer extends StatelessWidget {
@@ -12,22 +14,27 @@ class TableRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: AppColors.white,
-          border: Border.all(width: 1, color: AppColors.grey),
-          borderRadius: BorderRadius.circular(12)),
+          border: Border.all(width: 2, color: AppColors.primary),
+          borderRadius: BorderRadius.circular(10)),
       child: Table(
           border: TableBorder.symmetric(
-              inside: const BorderSide(width: 1, color: AppColors.grey)),
-          children: table.rows
-              .map((row) => TableRow(
-                  children: row.cells
-                      .map((cell) => Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 10),
-                            child: TheoryItemRenderer(content: cell.content),
-                          ))
-                      .toList()))
-              .toList()),
+              inside: const BorderSide(width: 2, color: AppColors.primary)),
+          children: _buildRows(table.rows)),
     );
+  }
+
+  List<TableRow> _buildRows(List<TheoryTableRow> rows) {
+    return rows
+        .map((row) => TableRow(children: _buildCells(row.cells)))
+        .toList();
+  }
+
+  List<Widget> _buildCells(List<TheoryTableCell> cells) {
+    return cells
+        .map((cell) => Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: TheoryItemRenderer(content: cell.content),
+            ))
+        .toList();
   }
 }
