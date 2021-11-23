79ab8aa0b4ac8f697186fc76542e5a2445df22fc
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-23 11:47:19 +0600

    refactor: remove one choice questions

--- lib/data/models/questions/one_choice_question.dart
+++
@@ -1,6 +0,0 @@
-import 'package:nomad/data/models/questions/question.dart';
-
-class OneChoiceQuestion extends Question {
-  const OneChoiceQuestion({required text, required answer})
-      : super(text: text, answer: answer);
-}
--- lib/units/components/renderers/questions/questions_renderer.dart
+++ lib/units/components/renderers/questions/questions_renderer.dart
@@ -1,13 +1,11 @@
 import "package:flutter/cupertino.dart";
 import 'package:nomad/data/models/questions/input_question.dart';
 import 'package:nomad/data/models/questions/match_question.dart';
-import 'package:nomad/data/models/questions/one_choice_question.dart';
 import 'package:nomad/data/models/questions/question.dart';
 import 'package:nomad/data/models/questions/true_false_question.dart';
 import 'package:nomad/units/components/renderers/questions/input_questions_renderer.dart';
 import 'package:nomad/units/components/renderers/questions/match_questions_renderer.dart';
 import 'package:nomad/units/components/renderers/questions/true_false_questions_renderer.dart';
-import 'package:nomad/units/components/renderers/questions/one_choice_questions_renderer.dart';
 
 typedef AnswersUpdateAction = Function(List<dynamic>);
 typedef AnswerUpdateAction = Function(int index, dynamic value);
@@ -34,19 +32,17 @@ class QuestionsRenderer extends StatelessWidget {
             answers: answers,
             onAnswerUpdate: onAnswerUpdate,
           )
-        : questions[0] is OneChoiceQuestion
-            ? OneChoiceQuestionsRenderer(questions: questions)
-            : questions[0] is MatchQuestion
-                ? MatchQuestionsRenderer(
+        : questions[0] is MatchQuestion
+            ? MatchQuestionsRenderer(
+                questions: questions,
+                onAnswersUpdate: onAnswersUpdate,
+              )
+            : questions[0] is TrueFalseQuestion
+                ? TrueFalseQuestionsRenderer(
                     questions: questions,
-                    onAnswersUpdate: onAnswersUpdate,
+                    answers: answers,
+                    onAnswerUpdate: onAnswerUpdate,
                   )
-                : questions[0] is TrueFalseQuestion
-                    ? TrueFalseQuestionsRenderer(
-                        questions: questions,
-                        answers: answers,
-                        onAnswerUpdate: onAnswerUpdate,
-                      )
-                    : Container();
+                : Container();
   }
 }
