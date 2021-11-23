090c389d1d8aff42a180a30821d9fb5307aac87b
Crepiks <sayazhan.onlassyn@mail.ru>
2021-11-23 22:36:58 +0600

    feat: display score of the current user in leaderboard view

--- lib/analytics/views/leaderboard_view.dart
+++ lib/analytics/views/leaderboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nomad/analytics/components/leaderboard_card.dart';
+import 'package:nomad/store/score.dart';
 
 class LeaderboardView extends StatefulWidget {
   const LeaderboardView({Key? key}) : super(key: key);
@@ -9,55 +10,42 @@ class LeaderboardView extends StatefulWidget {
 }
 
 class _LeaderBoardViewState extends State<LeaderboardView> {
+  int score = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadScore();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
-        children: const [
-          SizedBox(height: 60),
+        children: [
+          const SizedBox(height: 60),
           LeaderboardCard(
               active: true,
               name: "Саяжан Онласын",
               location: "Нур-Султан, Казахстан",
-              rating: 40),
-          SizedBox(
+              rating: score),
+          const SizedBox(
             height: 10,
           ),
           LeaderboardCard(
               name: "Саяжан Онласын",
               location: "Нур-Султан, Казахстан",
-              rating: 70),
-          SizedBox(
-            height: 10,
-          ),
-          LeaderboardCard(
-              name: "Саяжан Онласын",
-              location: "Нур-Султан, Казахстан",
-              rating: 40),
-          SizedBox(
-            height: 10,
-          ),
-          LeaderboardCard(
-              name: "Саяжан Онласын",
-              location: "Нур-Султан, Казахстан",
-              rating: 30),
-          SizedBox(
-            height: 10,
-          ),
-          LeaderboardCard(
-              name: "Саяжан Онласын",
-              location: "Нур-Султан, Казахстан",
-              rating: 20),
-          SizedBox(
-            height: 10,
-          ),
-          LeaderboardCard(
-              name: "Саяжан Онласын",
-              location: "Нур-Султан, Казахстан",
-              rating: 6),
+              rating: score),
         ],
       ),
     );
+  }
+
+  _loadScore() async {
+    final score = await getScore();
+    setState(() {
+      this.score = score;
+    });
   }
 }
