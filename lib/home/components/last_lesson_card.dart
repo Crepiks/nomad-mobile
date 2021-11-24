62aaaac06d072cb5448f9010fdf0abacf5c40a9d
Azatyk <azatuk2005@mail.ru>
2021-11-24 09:06:19 +0600

    feat: add last lesson card to home page

---
+++ lib/home/components/last_lesson_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import "package:flutter/material.dart";
+import 'package:nomad/common/components/common_button.dart';
+import 'package:nomad/common/constants/app_colors.dart';
+import 'package:nomad/data/models/unit.dart';
+import 'package:nomad/units/views/unit_view.dart';
+
+class LastLessonCard extends StatelessWidget {
+  const LastLessonCard({Key? key, required this.units}) : super(key: key);
+
+  final List<Unit> units;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      decoration: const BoxDecoration(
+          color: AppColors.primary,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          image: DecorationImage(
+              image: AssetImage("assets/images/last-lesson-card-image.png"),
+              scale: 3.2,
+              alignment: Alignment(1, 2.4))),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Пройдите урок и заработайте огоньки",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Ваш последний урок:",
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(height: 1.2, fontSize: 16),
+              ),
+              const SizedBox(height: 4),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: const Text(
+                  "Окончания — Урок 4. Мын/Сын/Сыз",
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      height: 1.2, fontSize: 16, fontStyle: FontStyle.italic),
+                ),
+              ),
+              const SizedBox(height: 14),
+              CommonButton(
+                text: "К уроку",
+                reverseColor: true,
+                onClick: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (context) =>
+                              UnitView(units: units, index: 0)));
+                },
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -4,19 +4,21 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nomad/common/components/action_button.dart';
 import 'package:nomad/common/constants/app_colors.dart';
 import 'package:nomad/common/renderers/score_comparison.dart';
+import 'package:nomad/data/mock/mock.dart';
+import 'package:nomad/data/models/chapter.dart';
 import 'package:nomad/data/models/user.dart';
 import 'package:nomad/data/models/user_score.dart';
 import 'package:nomad/home/components/home_header.dart';
+import 'package:nomad/home/components/last_lesson_card.dart';
 import 'package:nomad/home/components/progress_chart.dart';
-import 'package:nomad/home/components/repeat_card.dart';
 import 'package:nomad/store/score.dart';
 
 typedef ActiveTabChange = void Function(int index);
-  
+
 class HomeView extends StatefulWidget {
   const HomeView({Key? key, required this.navigateToPage, required this.user})
       : super(key: key);
-      
+
   final Function navigateToPage;
   final User user;
 
@@ -35,6 +37,8 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    List<Chapter> chapters = MockChapters.findAll();
+
     return ListView(
       children: [
         const SizedBox(height: 10),
@@ -62,42 +66,10 @@ class _HomeViewState extends State<HomeView> {
         const SizedBox(
           height: 20,
         ),
-        const Padding(
-          padding: EdgeInsets.only(right: 20, left: 20),
-          child: Text(
-            "Повторение",
-            style: TextStyle(
-                color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),
-          ),
-        ),
-        const SizedBox(
-          height: 14,
-        ),
-        SizedBox(
-          height: 180,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              const SizedBox(width: 20),
-              GestureDetector(
-                onTap: () => {},
-                child: const RepeatCard(
-                  title: "Местоимения",
-                  description:
-                      "самостоятельная часть речи, которая указывает на предметы, признаки, количество, но не называет их",
-                ),
-              ),
-              const SizedBox(width: 14),
-              GestureDetector(
-                onTap: () {},
-                child: const RepeatCard(
-                  title: "Местоимения",
-                  description:
-                      "самостоятельная часть речи, которая указывает на предметы, признаки, количество, но не называет их",
-                ),
-              ),
-              const SizedBox(width: 20),
-            ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: LastLessonCard(
+            units: chapters[0].units,
           ),
         )
       ],
@@ -116,8 +88,7 @@ buildScoreSheet(BuildContext context, Function navigateToPage) {
   List<UserScore> leaderboardUsers = [
     UserScore(
         "Айтбеков Сапабек", "assets/images/user-score-avatar-2.png", 80, false),
-    UserScore(
-        "Онласын Саяжан", "assets/images/mock-user-avatar.png", 60, true),
+    UserScore("Онласын Саяжан", "assets/images/mock-user-avatar.png", 60, true),
     UserScore(
         "Каримов Санжар", "assets/images/user-score-avatar-3.png", 50, false),
   ];
